0480ca6233fc7632d36004b1846765c08d2e9481
Ivan <ivannz@yandex.ru>
2019-09-27 00:03:36 +0300

    fixed package name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup, Extension
 
 setup(
-    name="dpdchallenge",
+    name="rfweblab",
     version="0.1",
-    description="""A pythonic interface to dpdchallenge.com.""",
+    description="""A pythonic interface to RFWebLab dpdchallenge.com.""",
     license="MIT License",
     author="Ivan Nazarov",
     author_email="ivan.nazarov@skolkovotech.ru",
     packages=[
-        "dpdchallenge",
+        "rfweblab",
     ],
     requires=["numpy", "urllib3"]
 )
