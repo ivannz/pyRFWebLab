945e22399f5272f9cc3ba0057ec7022b59f91703
Ivan <ivannz@yandex.ru>
2019-09-27 18:48:36 +0300

    added verbosity option

--- rfweblab/retry.py
+++ rfweblab/retry.py
@@ -14,7 +14,7 @@ def parameterized(decorator):
 
 
 @parameterized
-def retry_on(fn_call, exception, tries=2, delay=3, growth=1.5):
+def retry_on(fn_call, exception, tries=10, delay=2, growth=1.5, verbose=False):
     @wraps(fn_call)
     def wrapper(*args, **kwargs):
         n_tries, f_delay = tries, delay
@@ -23,6 +23,9 @@ def retry_on(fn_call, exception, tries=2, delay=3, growth=1.5):
                 return fn_call(*args, **kwargs)
 
             except exception as e:
+                if verbose:
+                    print(f"Retry in {f_delay} sec.")
+
                 time.sleep(f_delay)
                 n_tries, f_delay = n_tries - 1, f_delay * growth
 
